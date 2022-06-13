3b71547777e4d81454918427ccb3dbeef6f0e904
Swapnil Santosh Gite <gitesmait@gmail.com>
2022-06-13 12:40:30 +0530

    Notice Ui changed

--- app/src/main/java/com/example/trystudentapp/MainActivity.java
+++ app/src/main/java/com/example/trystudentapp/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.trystudentapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,13 +14,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.example.trystudentapp.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
