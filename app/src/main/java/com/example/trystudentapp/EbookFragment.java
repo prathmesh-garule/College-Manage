1bbec66cf7ef87e919d91d618cb747cd87e87042
Swapnil Santosh Gite <gitesmait@gmail.com>
2022-06-16 03:35:42 +0530

    E-Book

--- app/src/main/java/com/example/trystudentapp/EbookFragment.java
+++ app/src/main/java/com/example/trystudentapp/EbookFragment.java
@@ -2,63 +2,79 @@ package com.example.trystudentapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EbookFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class EbookFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private RecyclerView ebookRecycler;
+    private DatabaseReference reference;
+    private List<EbookData> list;
+    private EbookAdapter adapter;
+    private ProgressBar pd;
     public EbookFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EbookFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EbookFragment newInstance(String param1, String param2) {
-        EbookFragment fragment = new EbookFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ebook, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_ebook, container, false);
+        // Inflate the layout for this fragment
+
+        ebookRecycler = view.findViewById(R.id.ebookRecycler);
+        reference = FirebaseDatabase.getInstance().getReference().child("pdf");
+        pd = view.findViewById(R.id.ProgressBar);
+
+        ebookRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        ebookRecycler.setHasFixedSize(true);
+
+        getData();
+        return view;
+    }
+
+    private void getData() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list = new ArrayList<>();
+                for(DataSnapshot snapshot1:snapshot.getChildren()){
+                    EbookData data = snapshot1.getValue(EbookData.class);
+                    list.add(data);
+                }
+                adapter = new EbookAdapter(getContext(),list);
+                //ebookRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+                pd.setVisibility(View.GONE);
+                ebookRecycler.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
